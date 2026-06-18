eeb8de0e6dc8c21d446d3a38b5316649e0eca10a
Pranav Garg <pranavgarg.cs@gmail.com>
2026-06-19 00:14:47 +0530

    Added sqrt check for 3 mod 4 case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+pub mod primality_test;
+use crate::primality_test::check_prime_3_mod_4;
+use ibig::{ibig, modular::ModuloRing, ubig, UBig};
+use rand::prelude::*;
 fn main() {
-    let x = 0;
-    println!("Hello World!");
-    println!("{x}");
+    check_prime_3_mod_4(ubig!(103));
 }
--- src/primality_test.rs
+++ src/primality_test.rs
@@ -1,3 +1,19 @@
-use ibig::{ubig, UBig};
-use rand::Rng;
-use rayon::prelude::*;
+use std::ops::{Add, Shr};
+
+//implements the primality testing algorithm for 3 mod 4 and 1 mod 4 cases. 
+//Runs K tests, and returns majority output.
+use rand::{distributions::uniform::Uniform, thread_rng, Rng};
+use ibig::{UBig, ibig, modular::{IntoModulo, ModuloRing}, ubig};
+
+pub fn check_prime_3_mod_4(n: UBig) {
+    let ring = ModuloRing::new(&n);
+    let b: UBig = thread_rng().sample(Uniform::new(ubig!(1), &n - 1));
+    println!("Random base: {}", b);
+    let b = ring.from(b);
+    let s = b.pow(&ubig!(2));
+    println!("Quadratic Residue: {}", s.residue());
+    let exp = &n.add(ubig!(1)).shr(2);
+    let c = s.pow(exp);
+    println!("My Exponent: {}", exp);
+    println!("Answer: {}", c.residue());
+}
