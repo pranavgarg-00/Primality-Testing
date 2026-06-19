b135cef0d3712eea72e3e2c1a6c5b519a392337b
Pranav Garg <pranavgarg.cs@gmail.com>
2026-06-19 19:19:17 +0530

    Finished 3 mod 4 case
    Finished an unoptimized version of the 3 mod 4 case. I am able to generate 4096 bit primes though it takes about 30 seconds.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 pub mod primality_test;
-use crate::primality_test::check_prime_3_mod_4;
+use crate::primality_test::{check_prime_3_mod_4, generate_random_prime_3_mod_4, generate_random_ubig_3_mod_4};
 use ibig::{ibig, modular::ModuloRing, ubig, UBig};
 use rand::prelude::*;
+use std::ops::Add;
+
+
 fn main() {
-    check_prime_3_mod_4(ubig!(103));
+   println!("{}", generate_random_prime_3_mod_4(0b1000000000000_usize));
 }
--- src/primality_test.rs
+++ src/primality_test.rs
@@ -5,15 +5,43 @@ use std::ops::{Add, Shr};
 use rand::{distributions::uniform::Uniform, thread_rng, Rng};
 use ibig::{UBig, ibig, modular::{IntoModulo, ModuloRing}, ubig};
 
-pub fn check_prime_3_mod_4(n: UBig) {
-    let ring = ModuloRing::new(&n);
-    let b: UBig = thread_rng().sample(Uniform::new(ubig!(1), &n - 1));
-    println!("Random base: {}", b);
-    let b = ring.from(b);
-    let s = b.pow(&ubig!(2));
-    println!("Quadratic Residue: {}", s.residue());
-    let exp = &n.add(ubig!(1)).shr(2);
-    let c = s.pow(exp);
-    println!("My Exponent: {}", exp);
-    println!("Answer: {}", c.residue());
+pub fn check_prime_3_mod_4(n: UBig, k: i32) -> bool {
+    let ring: ModuloRing = ModuloRing::new(&n);
+    for _i in 0..k {
+        let b: UBig = thread_rng().sample(Uniform::new(ubig!(1), &n - 1));
+        // println!("Random base: {}", b);
+        let b = ring.from(b);
+        let s = b.pow(&ubig!(2));
+        // println!("Quadratic Residue: {}", s.residue());
+        let exp = &n.clone().add(ubig!(1)).shr(2);
+        let c = s.pow(exp);
+        // println!("My Exponent: {}", exp);
+        // println!("Answer: {}", c.residue());
+        if c != b && c != -b { 
+            return false;
+        }
+    }
+    return true;
+}
+
+pub fn generate_random_ubig_3_mod_4(num_bits: usize) -> UBig {
+    let mut large_num = ubig!(0);
+    let mut my_rng = thread_rng();
+    large_num.set_bit(0);
+    large_num.set_bit(1);
+    for i in 2..num_bits {
+        if my_rng.gen_bool(0.5) {
+            large_num.set_bit(i);
+        }
+    }
+    return large_num;
+}
+
+pub fn generate_random_prime_3_mod_4(num_bits: usize) -> UBig {
+    loop {
+        let a = generate_random_ubig_3_mod_4(num_bits);
+        if check_prime_3_mod_4(a.clone(), 100) {
+            return a;
+        }
+    }
 }
